cf99a4ae32ccc0602cd26a4559c5aa8010d417a9
Sheperd <111992706+sunmoumou1@users.noreply.github.com>
2024-02-23 21:30:09 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2ac9573f22514dce8f6bf97e7757f6a5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'sunmoumou resume',
+  domain: 'https://loving-railway-8e8.notion.site/Resume-2ac9573f22514dce8f6bf97e7757f6a5',
+  author: 'sunmoumou',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
